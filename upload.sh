a1f092f063fb9acac72c3732bf7e7a7bd13dedaf
Manuel Dipolt <manuel@linux-home.at>
2023-05-22 22:39:44 +0200

    0.0.1

--- upload.sh
+++ upload.sh
@@ -4,4 +4,4 @@ python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade build
 python -m build
 python3 -m pip install --upgrade twine
-python3 -m twine upload --repository testpypi dist/*
+python3 -m twine upload dist/*
